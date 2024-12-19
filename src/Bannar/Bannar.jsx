78bbe083fe3cca923349cee150b098832e255156
Sreedhar-roy <sreedharroy0@gmail.com>
2024-12-19 12:03:02 +0600

    bannar section and top bannar done

--- src/Bannar/Bannar.jsx
+++ src/Bannar/Bannar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import * as motion from "motion/react-client"
+import team1 from '../assets/team/happyTeam(1).jpg'
+import team2 from '../assets/team/happyTeam(2).jpg'
 
 const Bannar = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className='flex-1'>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <motion.img
+                        src={team1}
+                        animate={{y:[50, 100, 50]}}
+                        transition={{duration:10, delay:5, repeat:Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600  shadow-2xl" />
+                <motion.img 
+                src={team2} 
+                animate={{x:[100, 150, 100], y:[20, 50, 20]}}
+                transition={{duration:10, repeat:Infinity}}
+                className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-blue-600 border-b-4 border-l-4" />
                 </div>
+                
                 <div>
                     <motion.h1
                         animate={{ x: 50 }}
